ebfdab5bcb97fb65aef49788d461975f0d3c5558
nawelbennejma <nawelbennejma@yahoo.fr>
2022-10-02 21:58:31 +0100

    heroku

--- src/main/java/com/sip/ams/services/UserService.java
+++ src/main/java/com/sip/ams/services/UserService.java
@@ -33,7 +33,7 @@ public class UserService {
 		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 		//user.setActive(0);
 		user.setActive(1);
-		Role userRole = roleRepository.findByRole("USER");
+		Role userRole = roleRepository.findByRole("ADMIN");
 		user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
 		
 		userRepository.save(user);
